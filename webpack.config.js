4bc2fc46f12d6ed935303eb7a40eec695b6fb590
hastyy <jpm.dias@campus.fct.unl.pt>
2018-08-03 22:35:52 +0100

    Webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack demo",
-        }),
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack demo"
+    })
+  ],
+  devServer: {
+    // Display only errors to reduce the amount of output.
+    stats: "errors-only",
+
+    overlay: true,
+
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+    open: true // Open the page in browser
+  }
 };

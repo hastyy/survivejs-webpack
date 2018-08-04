ca7a747cb246fb293053ce14c5b8e79b2341e3d7
hastyy <jpm.dias@campus.fct.unl.pt>
2018-08-04 22:34:10 +0100

    Bundle-splitting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "purecss";
 import "./main.scss";
+import "react";
+import "react-dom";
 import component from "./component";
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,20 @@ const productionConfig = merge([
       limit: 15000,
       name: "[name].[ext]"
     }
-  })
+  }),
+    {
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendor",
+                        chunks: "initial",
+                    },
+                },
+            },
+        },
+    },
 ]);
 
 const developmentConfig = merge([
